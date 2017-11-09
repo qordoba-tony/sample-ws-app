56ede7455d1872377ee6fe98b44d1db7705ab569
Tony <tntuyen@ucdavis.edu>
2017-11-09 12:55:31 -0800

    update auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,7 @@ const loginPath = 'https://app.qordobatest.com/api/login'
 
 export const fetchToken = () => (dispatch, getState, axios) => {
 	console.log('FETCHING TOKEN!');
-	return axios.put(loginPath, { username: 'may@qordoba.com', password: 'qpassfrontend25' }, { headers: { 'content-type': 'application/json' }})
+	return axios.put(loginPath, { username: '<<USERNAME>>', password: '<<PASSWORD>>' }, { headers: { 'content-type': 'application/json' }})
 		.then(({ data }) => {
 			console.log('FETCHED TOKEN:', data);
 			dispatch({ type: SET_TOKEN, payload: data });
